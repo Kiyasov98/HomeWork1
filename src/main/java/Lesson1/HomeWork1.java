3f4d9dd2b64fe551843edfc6050d9ef5161a347d
Kiyasov98 <kiyasov_98@mail.ru>
2022-11-09 17:52:39 +0300

    Initial commit

---
+++ src/main/java/Lesson1/HomeWork1.java
@@ -0,0 +1,52 @@
+package Lesson1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+//        System.out.println("Hello, World");
+
+        //printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    private static void checkSumSign(){
+        int varA = 9;
+        int varB = 5;
+        int sumAandB = varA + varB;
+
+        if (sumAandB >= 0){
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+
+    public static void printColor() {
+        int value = 100;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 & value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+    public static void compareNumbers() {
+        int a = 10;
+        int b = 100;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
